4ea695bf5c8bba8633b47798038599dc2fb57c1a
FedericoCapitani <federico.capitani@outlook.it>
2022-05-13 12:10:36 +0200

    add ads_container in header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
     
+    <header>
+      <div class="header_container">
+        <div class="ads_container">
+          <div class="ads">
+          <div class="ads_item">
+            we have a 95% Successfull Pass Rate!
+          </div>
+          <div class="ads_item">
+            Give us a call to book your tuition! 1-800-555-555
+          </div>
+        </div>
+        </div>
+        <div class="navbar">
+          
+        </div>
+      </div>
+    </header>
+
   </div>
 </template>
 
