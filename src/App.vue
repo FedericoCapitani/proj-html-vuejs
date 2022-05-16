1026e1d5e4c7af264b3461432d71c8aab1583792
FedericoCapitani <federico.capitani@outlook.it>
2022-05-16 12:01:50 +0200

    add brand icons

--- src/App.vue
+++ src/App.vue
@@ -48,46 +48,20 @@
 
     </main>
 
-    <footer>
-      <div class="footer_links d_flex">
-        <div class="footer_col">
-          <img src="@/assets/img/avada-drivers-logo-1x.png" alt="logo avada small">
-          <p class="info">Avada Driving School is the right choice for experienced drivers looking for a refresher course, or the brand new driver wanting to pass first time</p>
-        </div>
-        <div class="footer_col">
-          <h4>CONTACT DETAILS</h4>
-          <ul>
-            <li> <font-awesome-icon :icon="['fas', 'house']" /><span style="margin-left: 2px"> 12345 Nort Main Street, </span> <p>New York, NY 555555</p> </li>
-            <li> <font-awesome-icon :icon="['fas', 'phone']" /><span style="margin-left: 6px"> 1.800.555.6789</span> </li>
-            <li> <font-awesome-icon :icon="['fas', 'envelope']" /><span style="margin-left: 10px"> info@your-domain.com</span> </li>
-            <li> <font-awesome-icon :icon="['fas', 'link']" /><span style="margin-left: 4px"> Theme-Fusion.com</span> </li>
-          </ul>
-        </div>
-        <div class="footer_col">
-          <h4>COURSES</h4>
-          <ul>
-            <li class="courses"> <span>Pass Plus</span></li>
-            <li class="courses"> <span>Intensive Course</span></li>
-            <li class="courses"> <span>Automatic</span></li>
-            <li class="courses"> <span>Instructor Training</span></li>
-          </ul>
-        </div>
-        <div class="footer_col d_flex">
-          <img src="@/assets/img/footer-ad-grid.png" alt="ads grid" class="ads_footer">
-          <button class="btn_footer">BOOK NOW</button>
-        </div>
-      </div>
-    </footer>
+  <SiteFooter/>
+    
   </div>
 </template>
 
 <script>
 import SiteHeader from "@/components/HeaderComponent.vue"
+import SiteFooter from "@/components/FooterComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    SiteHeader
+    SiteHeader,
+    SiteFooter
   },
   
 }
--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
-import { faCircleRight as farCircleRight } from '@fortawesome/free-regular-svg-icons' 
+import { faCircleRight as farCircleRight } from '@fortawesome/free-regular-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brand-svg-icons' 
+import { faTwitter } from '@fortawesome/free-brand-svg-icons'
+import { faYoutube } from '@fortawesome/free-brand-svg-icons'
+import { faInstagram } from '@fortawesome/free-brand-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPhone, faHouse, faEnvelope, faLink, farCircleRight)
+library.add(faPhone, faHouse, faEnvelope, faLink, farCircleRight, faTwitter, faYoutube, faFacebookF, faInstagram)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
