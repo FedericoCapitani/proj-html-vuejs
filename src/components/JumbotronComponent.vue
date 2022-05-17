90c3b8ba4c7c5d0ec9a8ca9c57a217a39cf18b97
FedericoCapitani <federico.capitani@outlook.it>
2022-05-17 13:45:55 +0200

    add latest news and jumbo component

---
+++ src/components/JumbotronComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="jumbotron">
+        <div class="jumbo_icons">
+          <div class="icon d_flex column_center">
+            <font-awesome-icon :icon="['fas', 'folder']" id="folder" />
+            <p>Demos</p>
+          </div>
+          <div class="icon">
+            <div class="price">
+              <p id="dollar">$</p>
+              <p>39</p>
+            </div>
+            <p>On Sale</p>
+          </div>
+        </div>
+        <div class="jumbo_text">
+          <h2>
+            Drive with Avada
+          </h2>
+          <p> We offer the finest tuition money can buy</p>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "JumboComponent",
+    data(){
+
+    }
+}
+</script>
---
+++ src/components/LatestNewsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="latest_news">
+        <div class="news_container">
+          <h2>Latest News</h2>
+          <div class="container">
+            <div class="col_half">
+              <img src="@/assets/img/blog-choosecar-700x441.jpg" alt="news_img_1">
+              <h3>What Car to Start With?</h3>
+              <p class="small">February 7th 2019 | <span class="green">0 Comments</span></p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+            <div class="col_half">
+              <img src="@/assets/img/blogpost-10and2-700x441.jpg" alt="news_img_2">
+              <h3>What Car to Start With?</h3>
+              <p class="small">February 7th 2019 | <span class="green">0 Comments</span></p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+          </div>
+          <button class="news_btn">MORE FROM THE BLOG</button>
+        </div>
+        <div class="angle_bg"></div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "LatestNews",
+    data() {
+
+    }
+}
+</script>
